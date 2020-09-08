4f851988764b058ae2edc54be251c2c24291e6f3
Ewilan <ewilan@dotslashplay.it>
2020-09-08 20:01:28 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,19 @@
     <!-- <introduction :texts="texts" :socials="socials"></introduction> -->
     <hero />
     <features />
-    {{ apiData }}
+    <!-- {{ apiData }} -->
     <formations :formations="formations" />
-    <projects-list :projects="projects" />
+    <!-- <projects-list :projects="projects" /> -->
     <projects-alt />
-    <skills :skills="skills" />
+    <!-- <skills :skills="skills" /> -->
     <!-- <contact></contact> -->
     <v-footer />
   </div>
 </template>
 
 <script>
+/* eslint-disable vue/no-unused-components */
+// eslint-disable-next-line no-unused-vars
 import qs from 'qs'
 // import introLogo from '~/components/intro-logo.vue'
 import hero from '@/components/blocks/hero'
@@ -37,60 +39,73 @@ export default {
     projectsAlt,
     skills,
   },
-  async asyncData({ app, query, error }) {
-    try {
-      const [
-        formations,
-        skills,
-        projects,
-        // passionsApi,
-        // textsApi,
-        // socialsApi
-      ] = await Promise.all([
-        app.$axios.$get('formations'),
-        app.$axios.$get(
-          `skills?${qs.stringify({
-            categories:
-              'framework-librairies,langages-de-developpement,technologies-logiciels',
-            limit: '9',
-            shuffle: true,
-            favorite: true,
-          })}`
-        ),
-        app.$axios.$get(
-          `projects?${qs.stringify({
-            limit: '6',
-          })}`
-        ),
-        // app.$axios.$get('passions'),
-        // app.$axios.$get('texts'),
-        // app.$axios.$get('socials')
-      ])
-      console.log('bouh?')
-      console.log(formations)
-
-      return {
-        formations: formations.data,
-        skills: skills.data,
-        projects: projects.data,
-        // passions: passionsApi.data,
-        // texts: textsApi.data,
-        // socials: socialsApi.data
-      }
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log(error)
-
-      return {
-        formations: [],
-        skills: [],
-        projects: [],
-        passions: [],
-        texts: [],
-        socials: [],
-      }
-    }
+  asyncData({ app, params, error }) {
+    return app.$axios
+      .$get('formations')
+      .then((res) => {
+        console.log(res)
+        return {
+          formations: res.data,
+        }
+      })
+      .catch((e) => {
+        error({ statusCode: 404, message: 'Billet non trouvé' })
+      })
   },
+  // async asyncData({ app, query, error }) {
+  //   try {
+  //     const [
+  //       formations,
+  //       skills,
+  //       projects,
+  //       // passionsApi,
+  //       // textsApi,
+  //       // socialsApi
+  //     ] = await Promise.all([
+  //       app.$axios.$get('formations'),
+  //       app.$axios.$get(
+  //         `skills?${qs.stringify({
+  //           categories:
+  //             'framework-librairies,langages-de-developpement,technologies-logiciels',
+  //           limit: '9',
+  //           shuffle: true,
+  //           favorite: true,
+  //         })}`
+  //       ),
+  //       app.$axios.$get(
+  //         `projects?${qs.stringify({
+  //           limit: '6',
+  //         })}`
+  //       ),
+  //       // app.$axios.$get('passions'),
+  //       // app.$axios.$get('texts'),
+  //       // app.$axios.$get('socials')
+  //     ])
+  //     console.log('bouh?')
+  //     console.log(formations)
+
+  //     return {
+  //       formations: formations.data,
+  //       skills: skills.data,
+  //       projects: projects.data,
+  //       // passions: passionsApi.data,
+  //       // texts: textsApi.data,
+  //       // socials: socialsApi.data
+  //     }
+  //   } catch (error) {
+  //     // eslint-disable-next-line no-console
+  //     console.log(error)
+
+  //     return {
+  //       formations: [],
+  //       skills: [],
+  //       projects: [],
+  //       passions: [],
+  //       texts: [],
+  //       socials: [],
+  //     }
+  //   }
+  // },
   data() {
     return {
       api: 'https://portfolio.ewilan-riviere.com/api/portfolio/formations',
